40ae521f4d0142146f9ed81b7a7881e90ef66dcf
violetctl39 <violetctl39@gmail.com>
2024-12-16 14:35:24 +0800

    naive

--- C++/2.cpp
+++ C++/2.cpp
@@ -13,22 +13,36 @@ void write(int x){
 	if(x>9)write(x/10);
 	pc(x%10+48);
 }
-
-int qpow(int x,int y,int mod){
-	int res=1,tmp=y;x%=mod;
-	for(int i=0,bs=1;i<=30;++i){
-		if((y&bs)!=0){
-			write(bs),pc(' ');
-			write(x),pc('\n');
-			res=res*x%mod;
-		}
-		bs<<=1ll;x=x*x%mod;
-	}return res;
-}
-
+const int mod=998244353;
+int n,m;
+int a[100005];
+int f[100005],g[100005];
+struct node{
+	int l,r,f,g;
+}t[700];
+int cnt,p[100005];
 signed main(){
-	cout<<qpow(17,39,264)<<endl;
-	cout<<qpow(65,233,299)<<endl;
-	cout<<qpow(260,17,299)<<endl;
+	n=read(),m=read();
+	for(int i=1;i<=n;++i)a[i]=read();
+	int l=1;
+	while(l<m){
+		int r=m/(m/l);t[++cnt].l=l;
+		if(r<m)t[cnt].r=r;
+		else {t[cnt].r=m-1;break;}
+		l=r+1;
+	}
+	for(int i=1;i<=cnt;++i)
+		for(int j=t[i].l;j<=t[i].r;++j)
+			p[j]=i;
+	t[1].f=0;t[1].g=1;
+	for(int i=1;i<=n;++i){
+		f[i]=(f[i-1]*2+g[i-1]*a[i])%mod;g[i]=g[i-1]*2%mod;
+		for(int j=cnt;j;--j){
+			if((m-1)/t[j].l+1<=a[i])f[i]=(f[i]+t[j].f+t[j].g*a[i])%mod,g[i]=(g[i]+t[j].g)%mod;
+			else t[p[t[j].l*a[i]]].f+=t[j].f+t[j].g*a[i]%mod,t[p[t[j].l*a[i]]].g+=t[j].g;
+		}
+	}
+	for(int i=1;i<=n;++i)write(f[i]),pc('\n');
+
 	return 0;
 }
